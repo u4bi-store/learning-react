7cff5aca7b1d4557e38e99e1de5d2f308f5bf22a
u4bi <rootcode10@gmail.com>
2017-12-19 12:45:54 +0900

    Movie app - Added Iteration Movies component list

--- movie-app/src/app/App.js
+++ movie-app/src/app/App.js
@@ -3,28 +3,40 @@ import './App.css';
 
 import Movie from './movie/Movie';
 
-const movieTitles = [
-    '영화1',
-    '영화2',
-    '영화3',
-    '영화4'
-];
+// const movieTitles = [
+//     '영화1',
+//     '영화2',
+//     '영화3',
+//     '영화4'
+// ];
 
-const movieImages = [
-    'http://www.hellhoundmusic.com/wp-content/uploads/2014/09/Forrest-Gump.jpg',
-    'https://media-cache.cinematerial.com/p/500x/w6zpxfct/if-only-south-korean-poster.jpg',
-    'http://smugglersite.com/wp-content/uploads/2016/01/Article-Image-82.jpg',
-    'http://www.kayafm.co.za/wp-content/uploads/2016/09/pursuit-of-happyness-quote.png'
+// const movieImages = [
+//     'http://www.hellhoundmusic.com/wp-content/uploads/2014/09/Forrest-Gump.jpg',
+//     'https://media-cache.cinematerial.com/p/500x/w6zpxfct/if-only-south-korean-poster.jpg',
+//     'http://smugglersite.com/wp-content/uploads/2016/01/Article-Image-82.jpg',
+//     'http://www.kayafm.co.za/wp-content/uploads/2016/09/pursuit-of-happyness-quote.png'
+// ];
+
+const movies = [
+    { title : '포레스트 검프', poster : 'http://www.hellhoundmusic.com/wp-content/uploads/2014/09/Forrest-Gump.jpg' },
+    { title : '이프 온리',    poster : 'https://media-cache.cinematerial.com/p/500x/w6zpxfct/if-only-south-korean-poster.jpg' },
+    { title : '원스',        poster : 'http://smugglersite.com/wp-content/uploads/2016/01/Article-Image-82.jpg' },
+    { title : '행복을찾아서',  poster : 'http://www.kayafm.co.za/wp-content/uploads/2016/09/pursuit-of-happyness-quote.png' }
 ];
 
 class App extends Component {
 render() {
     return (
     <div className="App">
-        <Movie title={ movieTitles[0] } poster={ movieImages[0] } />
+        
+        { movies.map(v => {
+            return <Movie title={ v.title } poster={ v.poster } />
+        })}
+
+        {/* <Movie title={ movieTitles[0] } poster={ movieImages[0] } />
         <Movie title={ movieTitles[1] } poster={ movieImages[1] } />
         <Movie title={ movieTitles[2] } poster={ movieImages[2] } />
-        <Movie title={ movieTitles[3] } poster={ movieImages[3] } />
+        <Movie title={ movieTitles[3] } poster={ movieImages[3] } /> */}
     </div>
     );
 }
