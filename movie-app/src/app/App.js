4720a81d3095b25ce6d182eb83277a4eb9fd117e
u4bi <rootcode10@gmail.com>
2017-12-20 01:13:58 +0900

    Movie app - Fetch API Call

--- movie-app/src/app/App.js
+++ movie-app/src/app/App.js
@@ -17,6 +17,12 @@ class App extends Component {
         componentDidUpdate()        5  hide spinner
     */
 
+    /* JSON API
+        https://yts.am/api#list_movies
+    */
+
+    state = {};
+
     componentWillMount(){
         console.log('will mount');
     }
@@ -24,28 +30,12 @@ class App extends Component {
     componentDidMount(){
         console.log('did mount');
 
-        setTimeout(() => {
-            
-            this.setState({
-                name : 'update movie app'
-            });
+        fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+        .catch(err => console.error(err));
 
-            this.setState({
-                movies : [
-                    { title : '포레스트 검프', poster : 'http://www.hellhoundmusic.com/wp-content/uploads/2014/09/Forrest-Gump.jpg' },
-                    { title : '이프 온리',    poster : 'https://media-cache.cinematerial.com/p/500x/w6zpxfct/if-only-south-korean-poster.jpg' },
-                    { title : '원스',        poster : 'http://smugglersite.com/wp-content/uploads/2016/01/Article-Image-82.jpg' },
-                    { title : '행복을찾아서',  poster : 'http://www.kayafm.co.za/wp-content/uploads/2016/09/pursuit-of-happyness-quote.png' },
-                    { title : '킥에스', poster : 'https://www.bloggang.com/data/w/waveminator/picture/1271516014.jpg' }
-                ]
-            });
-
-        }, 2000);
     }
-
-    state = {
-        name   : 'movie app'
-    };
 
     _renderMovies() {
         const movies = this.state.movies.map( (v, index) => {
@@ -56,12 +46,10 @@ class App extends Component {
     }
 
     render() {
-        console.log('dis render');
+        console.log('did render');
         return (
         <div className="App">
-            
-            <p> { this.state.name } </p>
-
+        
             { this.state.movies ? this._renderMovies() : 'Loading' }
 
         </div>
